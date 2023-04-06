947889878d8e0cf5eef2df83371b9f3ccaecbdf1
hama-jsoh <juseok@hamagroups.io>
2023-04-06 17:45:32 +0900

    :memo: Update example

--- examples/request_deploy_aws.py
+++ examples/request_deploy_aws.py
@@ -1,26 +1,56 @@
-from infrabot.elements.sections import Button, MarkDown, Fields
-from infrabot.elements.actions import TimePicker, DatePicker
-from infrabot.elements.inputs import CheckBoxInput, RadioButtonInput, StaticSelectInput, PlainTextInput
-from infrabot.elements.header import Header
-from infrabot.elements.view import Modal
 from infrabot.elements import Blocks
+from infrabot.elements.view import Modal
+from infrabot.elements.sections import MarkDown
+from infrabot.elements.inputs import (
+    CheckBoxInput,
+    RadioButtonInput,
+    StaticSelectInput,
+    PlainTextInput,
+)
+from infrabot.elements.actions import DatePicker, TimePicker
 
 
 if __name__ == "__main__":
-    view = Modal(
+    modal = Modal(
         "My App",
         Blocks(
-            StaticSelectInput("Repository", "Select a repo", ['project-admin', 'project-web', 'project-api'], "repo"),
-            StaticSelectInput("Branch", "Select a branch", ['master', 'develop', 'beta'], "branch"),
-            RadioButtonInput("Type", ['prod', 'dev'], "type", optional=True),
-            CheckBoxInput("Stage", ['CodeBuild', 'CodeDeploy'], "stage", optional=True),
-            CheckBoxInput("Auto Scaling Group", ['Exist'], "asg", optional=True),
+            StaticSelectInput(
+                title="Repository",
+                text="Select a repo",
+                selectbtn_texts=["project-admin", "project-web", "project-api"],
+                opt_value="repo",
+            ),
+            StaticSelectInput(
+                title="Branch",
+                text="Select a branch",
+                selectbtn_texts=["master", "develop", "beta"],
+                opt_value="branch"
+            ),
+            RadioButtonInput(
+                title="Type",
+                radiobtn_texts=["prod", "dev"],
+                opt_value="type",
+                optional=True
+            ),
+            CheckBoxInput(
+                title="Stage",
+                checkboxes=["CodeBuild", "CodeDeploy"],
+                opt_value="stage",
+                optional=True
+            ),
+            CheckBoxInput(
+                title="Auto Scaling Group",
+                checkboxes=["Exist"],
+                opt_value="asg",
+                optional=True
+            ),
             PlainTextInput("ASG or EC2 Name", multiline=True, optional=True),
             MarkDown("*Due*"),
             DatePicker("Select a date"),
             TimePicker("Select time"),
-            PlainTextInput("Anything else you want to tell us?")
-        )
+            PlainTextInput("Anything else you want to tell us?"),
+        ),
     )
-    import pdb;pdb.set_trace();
-    print(view.body)
+
+    modal.pretty
+    print(modal.to_dict)
