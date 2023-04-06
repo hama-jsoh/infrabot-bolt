a603c418aa3184a494fb421abd8a4a786c72839c
hama-jsoh <juseok@hamagroups.io>
2023-04-06 17:30:32 +0900

    :bug: Fix bugs

--- examples/request_deploy_aws.py
+++ examples/request_deploy_aws.py
@@ -3,21 +3,24 @@ from infrabot.elements.actions import TimePicker, DatePicker
 from infrabot.elements.inputs import CheckBoxInput, RadioButtonInput, StaticSelectInput, PlainTextInput
 from infrabot.elements.header import Header
 from infrabot.elements.view import Modal
+from infrabot.elements import Blocks
 
 
 if __name__ == "__main__":
     view = Modal(
         "My App",
-        StaticSelectInput("Select a repo", "repo list", "repo"),
-        StaticSelectInput("Select a branch", "branch list", "branch"),
-        RadioButtonInput("Type", ['prod', 'dev'], "type", optional=True),
-        CheckBoxInput("Stage", ['CodeBuild', 'CodeDeploy'], "stage", optional=True),
-        CheckBoxInput("Auto Scaling Group", ['Exist'], "asg", optional=True),
-        PlainTextInput("ASG or EC2 Name", multiline=True, optional=True),
-        MarkDown("*Due*"),
-        DatePicker("Select a date"),
-        TimePicker("Select time"),
-        PlainTextInput("Anything else you want to tell us?")
+        Blocks(
+            StaticSelectInput("Repository", ['project-admin', 'project-web', 'project-api'], "repo"),
+            StaticSelectInput("Branch", ['master', 'develop', 'beta'], "branch"),
+            RadioButtonInput("Type", ['prod', 'dev'], "type", optional=True),
+            CheckBoxInput("Stage", ['CodeBuild', 'CodeDeploy'], "stage", optional=True),
+            CheckBoxInput("Auto Scaling Group", ['Exist'], "asg", optional=True),
+            PlainTextInput("ASG or EC2 Name", multiline=True, optional=True),
+            MarkDown("*Due*"),
+            DatePicker("Select a date"),
+            TimePicker("Select time"),
+            PlainTextInput("Anything else you want to tell us?")
+        )
     )
-
-    print(view)
+    import pdb;pdb.set_trace();
+    print(view.body)
--- infrabot/elements/inputs/static_select_input.py
+++ infrabot/elements/inputs/static_select_input.py
@@ -2,12 +2,13 @@ from ._input import Input
 
 
 class StaticSelectInput(Input):
-    def __init__(self, title: str, selectbtn_texts: list, opt_value: str) -> None:
+    def __init__(self, title: str, text: str, selectbtn_texts: list, opt_value: str) -> None:
         super(StaticSelectInput, self).__init__()
         _type = "static_select"
         self.selections = selectbtn_texts
         _options = self.get_opts(opt_value)
-        _element = dict(type=_type, options=_options, action_id="static-select-action")
+        _placeholder = dict(type="plain_text", text=text, emoji=True)
+        _element = dict(type=_type, placeholder=_placeholder, options=_options, action_id="static-select-action")
         _label = dict(type="plain_text", text=title, emoji=True)
         self.body = dict(type=self.type, element=_element, label=_label)
 
